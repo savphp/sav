c7233cd69f85df206e463654a7c50f3ee79aecd4
jetiny <86287344@qq.com>
2018-03-28 13:50:35 +0800

    add get & set & all

--- src/Context.php
+++ src/Context.php
@@ -5,15 +5,30 @@ namespace Sav;
 class Context {
 
   function __construct($sav) {
-    $this->sav = $sav;
+    $this->_sav = $sav;
+    $this->_datas = array();
   }
 
   function __get($name) {
-    return $this->sav->getInstance($this, $name);
+    return $this->_sav->getInstance($this, $name);
   }
 
   function __call($name, $args) {
-    return $this->sav->callMethod($this, $name, $args);
+    return $this->_sav->callMethod($this, $name, $args);
+  }
+
+  function set ($name, $value) {
+    $this->_datas[$name] = $value;
+  }
+
+  function get ($name) {
+    if (array_key_exists($name, $this->_datas)) {
+      return $this->_datas[$name];
+    }
+  }
+
+  function all () {
+    return $this->_datas;
   }
 
 }
